78980c3adfd00822d61ce33edf85023a0dbc61c0
Nirawit Pongpisut <144346077+iintime@users.noreply.github.com>
2024-05-01 18:04:08 +0700

    Create Q2: Find total spend of each customer, order by total spend descending.sql

---
+++ cafe-sales-analysis/Q2: Find total spend of each customer, order by total spend descending.sql
@@ -0,0 +1,7 @@
+--Q2: Find total spend of each customer, order by total spend descending.
+
+select inv.customer_id,
+       (sum(ite.price) * sum(inv.quantity)) as total_spend
+from Invoices as inv
+join Items as ite on ite.item_id = inv.item_id
+group by inv.customer_id;
